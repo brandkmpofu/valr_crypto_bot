b734424427f61ec904fae26c582eebb91e030617
brandkmpofu <127902494+brandkmpofu@users.noreply.github.com>
2023-05-21 22:59:08 +0200

    Add files via upload
    Correct the inaccurate calculation of the average buy price of previous transactions

--- valr_bot_2/functions/last_buy_price.py
+++ valr_bot_2/functions/last_buy_price.py
@@ -6,11 +6,12 @@ def last_buy_price (pd,db_connection,dt,pair,np):
     """
 
 
-    last_buy_query ="SELECT AVG(PRICE) FROM REQUESTED_TRADES RT  WHERE CURRENCY_PAIR IN ('{pair}') AND\
-    TRADE_TYPE = 'BUY' ORDER BY POSTING_TIMESTAMP DESC LIMIT 3".format(pair=pair)
+    last_buy_query ="WITH intermediate AS (SELECT * FROM REQUESTED_TRADES RT \
+    WHERE CURRENCY_PAIR IN ('{pair}') AND TRADE_TYPE = 'BUY' \
+    ORDER BY RT.POSTING_TIMESTAMP DESC LIMIT 3) SELECT AVG(it.PRICE)  FROM intermediate it ".format(pair=pair)
     
     avg_last_buy_price = pd.read_sql(last_buy_query,db_connection)
-    avg_last_buy_price['AVG(PRICE)'] = avg_last_buy_price['AVG(PRICE)'].astype(float)
-    avg_last_buy_price = avg_last_buy_price['AVG(PRICE)'].iloc[0]
+    avg_last_buy_price['AVG(it.PRICE)'] = avg_last_buy_price['AVG(it.PRICE)'].astype(float)
+    avg_last_buy_price = avg_last_buy_price['AVG(it.PRICE)'].iloc[0]
 
     return(avg_last_buy_price)
